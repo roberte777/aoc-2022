f8489b40c821bb504c33771e51750d5200b3b430
roberte777 <robewilkes@yahoo.com>
2022-12-05 02:59:33 -0600

    idk

---
+++ day-4/target/debug/deps/part_2-d616c4bf0aee183c.d
@@ -0,0 +1,5 @@
+/home/roberte777/coding/rust/aoc-2022/day-4/target/debug/deps/part_2-d616c4bf0aee183c.rmeta: src/bin/part_2.rs
+
+/home/roberte777/coding/rust/aoc-2022/day-4/target/debug/deps/part_2-d616c4bf0aee183c.d: src/bin/part_2.rs
+
+src/bin/part_2.rs:
